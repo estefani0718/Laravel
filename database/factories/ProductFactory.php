7ddef6443ad8d036dce2640112a233d56980a301
estefani0718 <eestefanisg@gmail.com>
2025-06-11 18:13:58 -0500

    comienzo desde cero , con adelantos de otras cosas al crearlo

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Images;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' =>fake()->word(),
+            'price' =>fake()->randomFloat(2,10,1000),
+            'stock'=>fake()->numberBetween(0,100),
+            'category_id'=>Category::all()->random()->id,
+            'image_id' =>Images::factory()->create()->id,
+        ];
+    }
+}
